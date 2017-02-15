c26651e74d240139ba9fb8e4e9327737297b7f72
Ivan Pidov <ivan.pidov@gmail.com>
2017-02-15 10:11:28 +0200

    Removed glue from the project

--- src/main.js
+++ src/main.js
@@ -1,25 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import glue from 'tick42-glue'
+import VueRouter from 'vue-router'
 import App from './App'
 import router from './router'
 /* eslint-disable no-new */
 
-/* global window */
-glue({
-  desktop: false,
-  gateway: {
-    protocolVersion: 1,
-    ws: 'ws://localhost:22037'
-  }
-}).then((glue) => {
-  window.glue = glue
+Vue.use(VueRouter)
 
-  new Vue({
-    el: '#app',
-    router,
-    template: '<App />',
-    components: { App }
-  })
+if (__DEV__) {
+  window.Vue = Vue
+}
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<App />',
+  components: { App }
 })
